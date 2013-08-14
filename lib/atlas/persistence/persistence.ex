89aad73f2d69519dabbd4f082515852538882722
Chris McCord <chris@chrismccord.com>
2013-08-13 20:57:28 -0400

    Improve Persistence docs.

--- lib/atlas/persistence/persistence.ex
+++ lib/atlas/persistence/persistence.ex
@@ -16,74 +16,19 @@ defmodule Atlas.Persistence do
         model.primary_key_value(record) != nil
       end
 
-      defp attributes_without_nil_primary_key(record, model) do
-        if model.primary_key_value(record) do
-          model.to_list(record)
-        else
-          record
-          |> model.to_list
-          |> Keyword.delete_first(model.primary_key)
-        end
-      end
+      @doc """
+      Updates the record within the `model.table` database table running model validations.
+      Keyword list of attributes can be provided corresponding to named schema fields
 
-      def to_set_sql(attributes) do
-        attributes
-        |> Keyword.keys
-        |> Enum.map(fn column -> "#{adapter.quote_column(column)} = ?" end)
-        |> Enum.join(", ")
-      end
+      Examples
 
-      def to_column_sql(attributes) do
-        attributes
-        |> Keyword.keys
-        |> Enum.map(fn column -> "#{adapter.quote_column(column)}" end)
-        |> Enum.join(", ")
-      end
+        iex> user = Repo.first User
+        iex> Repo.update(User, user, age: 10)
+        {:ok, User.Record[age: 10..]}
 
-      def to_prepared_update_sql(record, model) do
-        attributes = Atlas.Record.to_list(record)
-        prepared_sql = """
-        UPDATE #{adapter.quote_tablename(model.table)}
-        SET #{to_set_sql(attributes)}
-        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)} = ?
-        """
-
-        { prepared_sql, Keyword.values(attributes) ++ [model.primary_key_value(record)] }
-      end
-
-      def to_prepared_insert_sql(record, model) do
-        attributes = attributes_without_nil_primary_key(record, model)
-
-        prepared_sql = """
-        INSERT INTO #{adapter.quote_tablename(model.table)}
-        (#{to_column_sql(attributes)})
-        VALUES(?)
-        RETURNING #{adapter.quote_column(model.primary_key)}
-        """
-
-        { prepared_sql, [Keyword.values(attributes)] }
-      end
-
-      def to_prepared_delete_sql(query = Query[], model) do
-        ids = query |> all |> Enum.map(model.primary_key_value(&1))
-
-        prepared_sql = """
-        DELETE FROM #{adapter.quote_tablename(model.table)}
-        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)}
-        IN(?)
-        """
-
-        {prepared_sql, [ids]}
-      end
-      def to_prepared_delete_sql(record, model) do
-        prepared_sql = """
-        DELETE FROM #{adapter.quote_tablename(model.table)}
-        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)} = ?
-        """
-
-        {prepared_sql, [model.primary_key_value(record)]}
-      end
-
+        iex> Repo.update(User, user, age: 0)
+        {:error, User.Record[age: 0..], ["age must be between 1 and 150"]}
+      """
       def update(model, record, attributes // []) do
         if Enum.any?(attributes), do: record = record.update(attributes)
         case model.validate(record) do
@@ -105,6 +50,21 @@ defmodule Atlas.Persistence do
       #   end
       # end
 
+      @doc """
+      Inserts a new record in Repo's database into `model.table` provided Keyword list of attributes
+      or `model.Record` instance.
+
+      Examples
+
+        iex> Repo.create(User, age: 12)
+        {:ok, User.Record[age: 12...]}
+
+        iex> Repo.create(User, User.Recod.new(age: 18))
+        {:ok, User.Record[age: 18...]}
+
+        iex> Repo.create(User, age: 0)
+        {:error, User.Record[age: 0..], ["age must be between 1 and 150"]}
+      """
       def create(model, attributes) when is_list(attributes) do
         create model, model.Record.new(attributes)
       end
@@ -120,6 +80,15 @@ defmodule Atlas.Persistence do
         end
       end
 
+      @doc """
+      Deletes record from Repo's database in `model.table` matching record's primary key value.
+
+      Examples
+        iex> user = User.first
+        User.Record[id: 123]
+        iex> Repo.destroy User, user
+        {:ok, User.Record[id: nil]}
+      """
       def destroy(model, record) do
         {sql, args} = to_prepared_delete_sql(record, model)
         case Client.execute_prepared_query(sql, args, __MODULE__) do
@@ -128,6 +97,15 @@ defmodule Atlas.Persistence do
         end
       end
 
+      @doc """
+      Deletes all records from Repo's database in `model.table` matching records' primary key values
+
+      Examples
+        iex> trashed_users = User.where(archived: true) |> Repo.all
+        [User.Record[id: 123], User.Record[id: 124], User.Record[id: 125]...]
+        iex> Repo.destroy_all trashed_users
+        {:ok, []}
+      """
       def destroy_all(records, model) when is_list(records) do
         ids = Enum.map records, &model.primary_key_value(&1)
         destroy_all(model.where([{model.primary_key, ids}]))
@@ -135,6 +113,74 @@ defmodule Atlas.Persistence do
       def destroy_all(query = Query[]) do
         {sql, args} = to_prepared_delete_sql(query, query.model)
         {:ok, _} = Client.execute_prepared_query(sql, args, __MODULE__)
+      end
+
+      defp attributes_without_nil_primary_key(record, model) do
+        if model.primary_key_value(record) do
+          model.to_list(record)
+        else
+          record
+          |> model.to_list
+          |> Keyword.delete_first(model.primary_key)
+        end
+      end
+
+      defp to_set_sql(attributes) do
+        attributes
+        |> Keyword.keys
+        |> Enum.map(fn column -> "#{adapter.quote_column(column)} = ?" end)
+        |> Enum.join(", ")
+      end
+
+      defp to_column_sql(attributes) do
+        attributes
+        |> Keyword.keys
+        |> Enum.map(fn column -> "#{adapter.quote_column(column)}" end)
+        |> Enum.join(", ")
+      end
+
+      defp to_prepared_update_sql(record, model) do
+        attributes = Atlas.Record.to_list(record)
+        prepared_sql = """
+        UPDATE #{adapter.quote_tablename(model.table)}
+        SET #{to_set_sql(attributes)}
+        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)} = ?
+        """
+
+        { prepared_sql, Keyword.values(attributes) ++ [model.primary_key_value(record)] }
+      end
+
+      defp to_prepared_insert_sql(record, model) do
+        attributes = attributes_without_nil_primary_key(record, model)
+
+        prepared_sql = """
+        INSERT INTO #{adapter.quote_tablename(model.table)}
+        (#{to_column_sql(attributes)})
+        VALUES(?)
+        RETURNING #{adapter.quote_column(model.primary_key)}
+        """
+
+        { prepared_sql, [Keyword.values(attributes)] }
+      end
+
+      defp to_prepared_delete_sql(query = Query[], model) do
+        ids = query |> all |> Enum.map(model.primary_key_value(&1))
+
+        prepared_sql = """
+        DELETE FROM #{adapter.quote_tablename(model.table)}
+        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)}
+        IN(?)
+        """
+
+        {prepared_sql, [ids]}
+      end
+      defp to_prepared_delete_sql(record, model) do
+        prepared_sql = """
+        DELETE FROM #{adapter.quote_tablename(model.table)}
+        WHERE #{adapter.quote_tablename(model.table)}.#{adapter.quote_column(model.primary_key)} = ?
+        """
+
+        {prepared_sql, [model.primary_key_value(record)]}
       end
     end
   end
